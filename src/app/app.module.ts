f68d59c718396e1190c856b7fd6a5dc58d06d83f
JSimonDev <yuditbertoli@gmail.com>
2023-02-04 17:48:20 -0400

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
 // Plugins:
 import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 import { IonicStorageModule } from '@ionic/storage-angular';
